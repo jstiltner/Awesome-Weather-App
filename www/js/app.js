9d2b2cd68693675c584e1d88938df87324a7a670
Jason Stiltner <jasonstiltner@gmail.com>
2016-01-11 23:13:32 -0600

    icon added and functional

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'angular-skycons'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -37,6 +37,8 @@ angular.module('starter', ['ionic'])
       // res.data.currently.temperature = weather.temp;
       
       weather.temp = parseInt(res.data.currently.temperature); 
+      weather.icon = res.data.currently.icon;
+      console.log("weather.icon", weather.icon);
     });
   });
 weather.temp = "--"
