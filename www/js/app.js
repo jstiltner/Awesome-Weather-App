33f933e37874c8f0fc3c2ec492ffa7ac10353020
Jason Stiltner <jasonstiltner@gmail.com>
2016-01-13 11:35:50 -0600

    WUnderground implemented

--- www/js/app.js
+++ www/js/app.js
@@ -25,26 +25,40 @@ angular.module('starter', ['ionic', 'angular-skycons'])
 
 .controller( 'weatherCtrl', function($http){
     var weather = this;
-  navigator.geolocation.getCurrentPosition(function (geopos){
-    var lat = geopos.coords.latitude;
-    var lon = geopos.coords.longitude;
-    var apikey = 'd679dd009502745611a44ae9f8f35978';
-    var url = '/api/forecast/' + apikey + '/' + lat + ',' + lon;
+  // navigator.geolocation.getCurrentPosition(function (geopos){
+    // var lat = geopos.coords.latitude;
+    // var lon = geopos.coords.longitude;
+    // var apikey = 'd679dd009502745611a44ae9f8f35978';
+    var url = 'http://api.wunderground.com/api/db2e52b4bf0f042b/conditions/q/autoip.json';
+    // var url = '/api/forecast/' + apikey + '/' + lat + ',' + lon;
+    var url2 = 'http://api.wunderground.com/api/db2e52b4bf0f042b/forecast/q/autoip.json';
     
     $http.get(url).then(function(res){
-    
-      console.log("res", res );
+    console.log("res.data.current_observation", res.data.current_observation );
+      weather.temp = res.data.current_observation.temp_f;
+      weather.city = res.data.current_observation.display_location.city; 
+      weather.icon = res.data.current_observation.icon_url; 
+      
       // res.data.currently.temperature = weather.temp;
       
-      weather.temp = parseInt(res.data.currently.temperature); 
-      weather.icon = res.data.currently.icon;
+      // weather.temp = parseInt(res.data.currently.temperature); 
+      // weather.icon = res.data.currently.icon;
       console.log("weather.icon", weather.icon);
     });
+
+    $http.get(url2).then(function(res){
+      console.log("res2", res);
+    })
+
+
+
+
+
   });
-weather.temp = "--"
+// weather.temp = "--"
   
   // Could have used ng-resource call here, but Scott has warned us against this.
-});
+// });
 
 
 // .config(function ($stateProvider, $urlRouterProvider){
